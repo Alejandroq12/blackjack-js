a4cfc2f9ab56e399ad35a22373819f407270c33c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-04-14 22:09:52 -0600

    Chore: Properly indent code.

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -49,9 +49,7 @@ const getCard = () => {
 
 const cardValue = (card) => {
   const value = card.substring(0, card.length - 1);
-  return (isNaN(value))
-    ? (value === 'A') ? 11 : 10
-    : value * 1;
+  return isNaN(value) ? (value === 'A' ? 11 : 10) : value * 1;
 };
 
 // Computer turn

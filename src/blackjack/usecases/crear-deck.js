e3d2a8f08f498ac1067c2effd15f00b569641134
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-06-02 13:13:13 -0600

    Docs: Use JsDocs to add information about how what type of data a function it is supose to receive.

--- src/blackjack/usecases/crear-deck.js
+++ src/blackjack/usecases/crear-deck.js
@@ -1,6 +1,15 @@
   import _ from 'underscore';
 
+  /**
+   * This function creates a new deck.
+   * @param { Array<string> } typesOfCards Ejemplo: ['C', 'D', 'H', 'S']
+   * @param { Array<string> } especialTypes Ejemplo: ['A', 'J', 'Q', 'K']
+   * @returns { Array } It returns a new deck of cards.
+   */
   export const createDeck = (typesOfCards, especialTypes) => {
+    if(!typesOfCards || typesOfCards.length === 0) throw new Error('typesOfCards it is required as an array of strings!');
+    if(!especialTypes || especialTypes.length === 0) throw new Error('especialTypes it is required as an array of strings!');
+
     let deck = [];
     for (let i = 2; i <= 10; i += 1) {
       for (const tipo of typesOfCards) {

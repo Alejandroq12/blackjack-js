3efe0d821b53633049aaee61b84bbe453e513cd0
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-06-08 13:13:13 -0600

    Docs: Use JSDocs to document how the cardValue function works.

--- src/blackjack/usecases/card-value.js
+++ src/blackjack/usecases/card-value.js
@@ -1,3 +1,9 @@
+/**
+ * This function returns the value(number) of the card
+ * @param { String } card Example: '2C'
+ * @returns { Number } The number that will be displayed as the player or computer point.
+ */
+
 export const cardValue = (card) => {
   const value = card.substring(0, card.length - 1);
   return isNaN(value) ? (value === 'A' ? 11 : 10) : value * 1;

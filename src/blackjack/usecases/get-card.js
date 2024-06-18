1abfd0e442061f44bb1c42aaf260085712b3dfb5
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-06-18 13:48:59 -0600

    Docs: Use JSDocs to document how the getCard function works.

--- src/blackjack/usecases/get-card.js
+++ src/blackjack/usecases/get-card.js
@@ -1,3 +1,8 @@
+/**
+ * This function gets the last card of the deck array
+ * @param { Array<string> } deck - An Array of strings with card values
+ * @returns { String } The last card of the deck as a string
+ */
 export const getCard = (deck) => {
   if (deck.length === 0) {
     throw new Error('No hay cartas en el deck');
